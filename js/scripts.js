4546617ae75d82b70a44e914f200106507449b4d
stivi00 <stivisth@gmail.com>
2023-07-01 22:26:29 +0200

    added array with objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-alert('Hello world');
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        types: ['water'] 
+    }
+];
 
-let favoriteFood = "Sarma";
-
-document.write(favoriteFood);
