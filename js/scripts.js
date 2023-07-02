03e843e44d4690a9895d01b3c93f19c89c0abb5a
stivi00 <stivisth@gmail.com>
2023-07-02 22:30:12 +0200

    iterating over array of objects added

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,35 @@ let pokemonList = [
         types: ['grass', 'poison']
     },
     {
-        name: 'Charmander',
-        height: 0.6,
-        types: ['fire']
+        name: 'Charizard',
+        height: 1.7,
+        types: ['fire', 'flying']
     },
     {
         name: 'Squirtle',
         height: 0.5,
-        types: ['water'] 
+        types: ['water']
+    },
+    {
+        name: 'Onix',
+        height: 8.8,
+        types: ['rock', 'ground']
     }
 ];
 
+// for (i = 0; i < pokemonList.length; i++) {
+//     if (pokemonList[i].height > 1) {
+//         document.write(`${pokemonList[i].name} (${pokemonList[i].height}) - Wow, that's big! <br><br> `)
+//     } else {
+//         document.write(`${pokemonList[i].name} (${pokemonList[i].height}) <br><br> `)
+//     }
+// }
+
+
+for (i = 0; i < pokemonList.length; i++) {
+
+    pokemonList[i].height > 1 ? 
+        document.write(`${pokemonList[i].name} (${pokemonList[i].height}) - Wow, that's big! <br><br> `) : 
+        document.write(`${pokemonList[i].name} (${pokemonList[i].height}) <br><br> `)
+
+}
