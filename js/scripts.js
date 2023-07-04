6d2ad1f23a637c66fa2b3e5bf88d3ec6eb63b123
stivi00 <stivisth@gmail.com>
2023-07-04 14:07:33 +0200

    refactoring code to use functional programming paradigm

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,45 @@
-// let pokemonRepository = (function () {
-//     let pokemonList = [];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Charizard',
+            height: 1.7,
+            types: ['fire', 'flying']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            types: ['water']
+        },
+        {
+            name: 'Onix',
+            height: 8.8,
+            types: ['rock', 'ground']
+        }
+    ];
+    
 
-//     function add(pokemon){
-//         pokemonList.push(pokemon)
-//     }
-
-//     function getAll() {
-//         return pokemonList;
-//     }
-
-//     return {
-//         add: add,
-//         getAll: getAll
-//     }
-
-// })();
-
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Charizard',
-        height: 1.7,
-        types: ['fire', 'flying']
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        types: ['water']
-    },
-    {
-        name: 'Onix',
-        height: 8.8,
-        types: ['rock', 'ground']
+    function add(pokemon){
+        pokemonList.push(pokemon)
     }
-];
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    }
+
+})();
+
+let pokemonList = pokemonRepository.getAll()
 
 pokemonList.forEach(pokemon => {
-    document.write(pokemon.name) + document.write(pokemon.height) 
-
+    document.write(`${pokemon.name} (${pokemon.height}) <br><br> `)
 })
