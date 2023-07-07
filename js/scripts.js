5ab25fc30d5abe6e60d427dfbf2977d48ad99a10
stivi00 <stivisth@gmail.com>
2023-07-07 12:30:45 +0200

    logging pokemon name in console on click

--- js/scripts.js
+++ js/scripts.js
@@ -44,10 +44,14 @@ let pokemonRepository = (function () {
         button.classList.add('button-class');
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);
+
+        button.addEventListener('click', function(){
+            showDetails(pokemon)
+        })        
     }
 
     function showDetails(pokemon){
-        console.log(pokemon)
+        console.log(pokemon.name)
     }
 
     return {
