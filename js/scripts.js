faf04453fd66b57686ad8755b84c15020fd902e7
stivi00 <stivisth@gmail.com>
2023-08-06 19:40:54 +0200

    styling removed

--- js/scripts.js
+++ js/scripts.js
@@ -18,79 +18,63 @@ let pokemonRepository = (function () {
 
     function loadList() {
         return fetch(apiUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (json) {
-            json.results.forEach(function (item) {
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                json.results.forEach(function (item) {
 
-                let pokemon = {
-                    name: item.name,
-                    detailsUrl: item.url
-                };
-                add(pokemon);
-            });
-        })
-        .catch(function (e) {
-            console.error(e)
-        })
+                    let pokemon = {
+                        name: item.name,
+                        detailsUrl: item.url
+                    };
+                    add(pokemon);
+                });
+            })
+            .catch(function (e) {
+                console.error(e)
+            })
     }
 
 
     function loadDetails(item) {
         let url = item.detailsUrl;
         return fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (details) {
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (details) {
 
-            // detailed information about pokemon is extracted from details argument
+                // detailed information about pokemon is extracted from details argument
 
-            console.log(details)
-            item.imageUrl = details.sprites.other.dream_world.front_default;
-            item.height = details.height;
-            item.types = details.types;
-        })
-        .catch(function (e) {
-            console.error(e);
-        });
+                console.log(details)
+                item.imageUrl = details.sprites.other.dream_world.front_default;
+                item.height = details.height;
+                item.types = details.types;
+            })
+            .catch(function (e) {
+                console.error(e);
+            });
     }
 
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
 
-            //for some reason modal will show previously clicked pokemon for short time 
-
             const modalContainer = document.getElementById("modal-container");
             const modalTitle = document.getElementById("modal-title");
             const modalHeight = document.getElementById("modal-height");
             const modalImage = document.getElementById("modal-image");
-      
+
             modalTitle.textContent = "Name: " + pokemon.name;
             modalHeight.textContent = "Height: " + pokemon.height
-      
+
             modalImage.setAttribute("src", pokemon.imageUrl);
             modalImage.setAttribute("alt", pokemon.name);
-      
+
             modalContainer.style.display = "block";
 
         });
     }
-
-
-    // is this the most optimal way to hide modal?
-    // function hideModal() {
-    //     let modalContainer = document.querySelector('#modal-container');
-    //     modalContainer.classList.remove('is-visible');
-    // }
-
-    // window.addEventListener('keydown', (e) => {
-    //     let modalContainer = document.querySelector('#modal-container');
-    //     if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-    //         hideModal();
-    //     }
-    // });
 
 
     function addListItem(pokemon) {
@@ -100,17 +84,17 @@ let pokemonRepository = (function () {
         pokemonCard.classList.add("col-lg-3", "col-md-4", "col-sm-6", "mb-4", "pokemon-card"); // Set Bootstrap grid classes
         let button = document.createElement("button");
         button.innerText = pokemon.name;
-        
+
         button.classList.add("btn", "btn-primary", "w-100"); // Add Bootstrap utility classes
         button.setAttribute("data-target", "#modal-container"); // Set data-target attribute
         button.setAttribute("data-toggle", "modal"); // Set data-toggle attribute
-        
+
         pokemonCard.appendChild(button);
         pokemonRow.appendChild(pokemonCard);
         button.classList.add("rainbow-gradient-button");
 
         button.addEventListener("click", function (event) {
-          showDetails(pokemon);
+            showDetails(pokemon);
         });
     }
 
@@ -137,3 +121,13 @@ pokemonRepository.loadList().then(function () {
 
 });
 
+//FOR BUG WHERE MODAL CONTENT IS NOT XLEARED AFTER CLOSING 
+
+//apparently this code should work but doesn't
+
+// $(document).ready(function () {
+        
+//     $('.modal').on('hidden.bs.modal', function () {
+//         $(this).removeData('bs.modal');
+//     });
+// });
