1d5352d33b6844ae90077729c66d17fc2524e4e2
stivi00 <stivisth@gmail.com>
2023-08-11 17:21:00 +0200

    formatting code

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@ let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-
     function add(pokemon) {
-
         if (typeof pokemon === 'object') {
-            pokemonList.push(pokemon)
+            pokemonList.push(pokemon);
         } else {
-            document.write(`${pokemon} is not an object. Only object is accepted <br><br><br>`)
+            document.write(
+                `${pokemon} is not an object. Only object is accepted <br><br><br>`
+            );
         }
     }
 
@@ -23,19 +23,17 @@ let pokemonRepository = (function () {
             })
             .then(function (json) {
                 json.results.forEach(function (item) {
-
                     let pokemon = {
                         name: item.name,
-                        detailsUrl: item.url
+                        detailsUrl: item.url,
                     };
                     add(pokemon);
                 });
             })
             .catch(function (e) {
-                console.error(e)
-            })
+                console.error(e);
+            });
     }
-
 
     function loadDetails(item) {
         let url = item.detailsUrl;
@@ -44,7 +42,6 @@ let pokemonRepository = (function () {
                 return response.json();
             })
             .then(function (details) {
-
                 // detailed information about pokemon is extracted from details argument
 
                 item.imageUrl = details.sprites.other.dream_world.front_default;
@@ -58,43 +55,47 @@ let pokemonRepository = (function () {
 
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
-
             const modalContainer = document.getElementById('modal-container');
             const modalTitle = document.getElementById('modal-title');
             const modalHeight = document.getElementById('modal-height');
             const modalImage = document.getElementById('modal-image');
 
-            modalTitle.textContent = 'Name: '  + pokemon.name;
-            modalHeight.textContent = 'Height: '  + pokemon.height
+            modalTitle.textContent = 'Name: ' + pokemon.name;
+            modalHeight.textContent = 'Height: ' + pokemon.height;
 
             modalImage.setAttribute('src', pokemon.imageUrl);
             modalImage.setAttribute('alt', pokemon.name);
 
             modalContainer.style.display = 'block';
 
+            console.log('test');
         });
     }
-
 
     function addListItem(pokemon) {
         let pokemonRow = document.getElementById('pokemonRow');
         let pokemonCard = document.createElement('div');
         let pokemonName = document.createElement('h5');
-        let pokemonImage = document.createElement('img')
+        let pokemonImage = document.createElement('img');
 
-        pokemonImage.classList.add('pokemon-image')
-        pokemonCard.classList.add('card', 'pokemon-card', 'col-lg-3', 'col-md-4', 'col-sm-6', 'mb-4'); 
+        pokemonImage.classList.add('pokemon-image');
+        pokemonCard.classList.add(
+            'card',
+            'pokemon-card',
+            'col-lg-3',
+            'col-md-4',
+            'col-sm-6',
+            'mb-4'
+        );
 
-        loadDetails(pokemon).then(function(){
-
+        loadDetails(pokemon).then(function () {
             pokemonImage.src = pokemon.imageUrl;
-        })
+        });
 
         pokemonName.innerText = pokemon.name;
 
-
         pokemonCard.setAttribute('data-target', '#modal-container');
-        pokemonCard.setAttribute('data-toggle', 'modal'); 
+        pokemonCard.setAttribute('data-toggle', 'modal');
 
         pokemonCard.appendChild(pokemonName);
         pokemonCard.appendChild(pokemonImage);
@@ -106,7 +107,6 @@ let pokemonRepository = (function () {
         });
     }
 
-
     return {
         add: add,
         getAll: getAll,
@@ -114,17 +114,13 @@ let pokemonRepository = (function () {
         showDetails: showDetails,
         loadList: loadList,
         loadDetails: loadDetails,
-    }
-
+    };
 })();
 
-
-let allPokemons = pokemonRepository.getAll()
+let allPokemons = pokemonRepository.getAll();
 
 pokemonRepository.loadList().then(function () {
-
-    allPokemons.forEach(pokemon => {
-        pokemonRepository.addListItem(pokemon)
+    allPokemons.forEach((pokemon) => {
+        pokemonRepository.addListItem(pokemon);
     });
-
 });
